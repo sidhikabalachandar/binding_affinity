9516d27f77ef28f4392f02ed5d3c0d61d7d0eca7
sidhikabalachandar <sidhikab@stanford.edu>
2020-11-11 18:47:50 -0500

    muscaronic

--- search_bindingdb.py
+++ search_bindingdb.py
@@ -5,10 +5,16 @@ def main():
     prots = []
     for prot in df['Target Name Assigned by Curator or DataSource'].unique():
         prot_df = df[df['Target Name Assigned by Curator or DataSource'] == prot]
+        # index 0 is prot name, index 1 is number of distinct structures in binding db, index 2 is number of distinct
+        # structures in pdb
         prots.append((prot, len(prot_df), len(prot_df['PDB ID(s) for Ligand-Target Complex'].unique())))
 
     sorted_prots = sorted(prots, key=lambda x: x[2], reverse=True)
-    print(sorted_prots[:5])
+    top = sorted_prots[0]
+    top_df = df[df['Target Name Assigned by Curator or DataSource'] == top[0]]
+    pdb_names = top_df['PDB ID(s) for Ligand-Target Complex'].unique()
+    for name in pdb_names:
+        print(name)
 
 if __name__ == "__main__":
     main()
