443692818aaba77a9a806bf5f3e324ead4bd9b05
sidhikabalachandar <sidhikab@stanford.edu>
2020-11-11 19:55:42 -0500

    updating

--- search_bindingdb.py
+++ search_bindingdb.py
@@ -7,14 +7,17 @@ def main():
         prot_df = df[df['Target Name Assigned by Curator or DataSource'] == prot]
         # index 0 is prot name, index 1 is number of distinct structures in binding db, index 2 is number of distinct
         # structures in pdb
-        prots.append((prot, len(prot_df), len(prot_df['PDB ID(s) for Ligand-Target Complex'].unique())))
+        prots.append((prot, len(prot_df), len(prot_df['PDB ID(s) for Ligand-Target Complex'].unique()),
+                      len(prot_df['PubChem CID'].unique())))
 
-    sorted_prots = sorted(prots, key=lambda x: x[2], reverse=True)
+    sorted_prots = sorted(prots, key=lambda x: x[3], reverse=True)
+
+    # change the number down here to get more/less structures
+    print(sorted_prots[:5])
     top = sorted_prots[0]
     top_df = df[df['Target Name Assigned by Curator or DataSource'] == top[0]]
-    pdb_names = top_df['PDB ID(s) for Ligand-Target Complex'].unique()
-    for name in pdb_names:
-        print(name)
+    df = top_df.drop_duplicates(subset='PubChem CID', keep="first")
+    df.to_csv('serotonin_all.csv')
 
 if __name__ == "__main__":
     main()
